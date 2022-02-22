f63a1e289517c420f51d320a56bdd5dcfea913cf
PedroFolego <hmfpedro@hotmail.com>
2022-02-22 18:43:36 -0300

    header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1>Super Trunfo</h1>
+        <h1 className="header">
+          Super
+          <span>Trunfo</span>
+        </h1>
       </div>
     );
   }
